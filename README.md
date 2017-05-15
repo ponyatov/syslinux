068dcd1c153652d695df4cbe4ccf9050058a175d
Dmitry Ponyatov <dponyatov@gmail.com>
2017-05-15 10:53:50 +0400

    + hdimage boot + get.bat
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ```
 git clone -o gh https://github.com/ponyatov/syslinux.git F:\Linux
 F:\Linux\syslinux.exe -d Linux -i F:
+cp F:\Linux\syslinux.cfg F:\
 
-wget -P F:\Linux https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
-
-wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/amd64/iso-cd/debian-8.8.0-amd64-netinst.iso
-wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/i386/iso-cd/debian-8.8.0-i386-netinst.iso
+F:
+cd \Linux
+get.bat
 ```
---
+++ get.bat
@@ -0,0 +1,13 @@
+wget -P F:\Linux https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
+
+wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/amd64/iso-cd/debian-8.8.0-amd64-netinst.iso
+wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/i386/iso-cd/debian-8.8.0-i386-netinst.iso
+
+mkdir amd64
+wget -P amd64 http://cdimage.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/vmlinuz
+wget -P amd64 http://cdimage.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/initrd.gz
+
+mkdir i386
+wget -P i386 http://cdimage.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/vmlinuz
+wget -P i386 http://cdimage.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/initrd.gz
+
