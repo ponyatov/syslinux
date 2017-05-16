9de73140a126bd854d862a0863bb09b6a711a163
Dmitry Ponyatov <dponyatov@gmail.com>
2017-05-16 14:22:39 +0400

    * get
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- get.bat
+++ get.bat
@@ -1,7 +1,5 @@
-wget -P F:\Linux https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
 
-wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/amd64/iso-cd/debian-8.8.0-amd64-netinst.iso
-wget -P F:\Linux https://cdimage.debian.org/debian-cd/8.8.0/i386/iso-cd/debian-8.8.0-i386-netinst.iso
+wget --no-check-certificate https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
 
 mkdir amd64
 wget -P amd64 http://cdimage.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/vmlinuz
@@ -10,4 +8,9 @@ wget -P amd64 http://cdimage.debian.org/debian/dists/stable/main/installer-amd64
 mkdir i386
 wget -P i386 http://cdimage.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/vmlinuz
 wget -P i386 http://cdimage.debian.org/debian/dists/stable/main/installer-i386/current/images/hd-media/initrd.gz
+
+pause
+
+wget --no-check-certificate https://cdimage.debian.org/debian-cd/8.8.0/amd64/iso-cd/debian-8.8.0-amd64-netinst.iso
+wget --no-check-certificate https://cdimage.debian.org/debian-cd/8.8.0/i386/iso-cd/debian-8.8.0-i386-netinst.iso
 
